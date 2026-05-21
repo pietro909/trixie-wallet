c46f04a4f9d2e9068926f90dbaceb9b36709d0b0
Pietro Grandi <dev@pietro.uno>
2026-05-21 09:41:43 +0200

    Fix Jest transform for pnpm packages

--- jest.config.js
+++ jest.config.js
@@ -6,7 +6,7 @@ module.exports = {
     "^@/(.*)$": "<rootDir>/$1",
   },
   transformIgnorePatterns: [
-    "node_modules/(?!((jest-)?react-native|@react-native(-community)?|expo(nent)?|@expo(nent)?/.*|@expo-google-fonts/.*|react-clone-referenced-element|@react-navigation/.*|@unimodules/.*|sentry-expo|native-base|react-native-svg|@arkade-os/.*|@scure/.*|@noble/.*|micro-packed|uint8array-tools|varuint-bitcoin|@bitcoinerlab|@marcbachmann/cel-js))",
+    "node_modules/(?!\\.pnpm/|((jest-)?react-native|@react-native(-community)?|expo(nent)?|@expo(nent)?/.*|@expo-google-fonts/.*|react-clone-referenced-element|@react-navigation/.*|@unimodules/.*|sentry-expo|native-base|react-native-svg|@arkade-os/.*|@scure/.*|@noble/.*|micro-packed|uint8array-tools|varuint-bitcoin|@bitcoinerlab|@marcbachmann/cel-js))",
   ],
   collectCoverageFrom: [
     "app/services/arkade/activity-history.ts",
